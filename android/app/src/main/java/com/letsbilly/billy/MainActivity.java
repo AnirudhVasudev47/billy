0ecaa7c833963881a968b34cc1ca22d5a5a96d93
AnirudhVasudev47 <69135996+AnirudhVasudev47@users.noreply.github.com>
2021-06-02 23:49:56 +0530

    Setup Complete

--- android/app/src/main/java/com/letsbilly/billy/MainActivity.java
+++ android/app/src/main/java/com/letsbilly/billy/MainActivity.java
@@ -1,6 +1,25 @@
 package com.letsbilly.billy;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory;
+
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FirebaseApp.initializeApp(this);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(
+                SafetyNetAppCheckProviderFactory.getInstance());
+
+    }
 }
