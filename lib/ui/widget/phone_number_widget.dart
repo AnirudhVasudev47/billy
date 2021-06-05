11e2327abca86371185b74331d653d710df494e6
AnirudhVasudev47 <69135996+AnirudhVasudev47@users.noreply.github.com>
2021-06-06 00:30:13 +0530

    Small change

--- lib/ui/widget/phone_number_widget.dart
+++ lib/ui/widget/phone_number_widget.dart
@@ -1,5 +1,4 @@
 import 'package:billy/global/color.dart';
-import 'package:billy/ui/screen/enter_number_screen.dart';
 import 'package:billy/ui/screen/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
@@ -61,8 +60,15 @@ class _PhoneAuthWidgetState extends State<PhoneAuthWidget> {
         showLoading = false;
       });
 
-      _scaffoldKey.currentState!
-          .showSnackBar(SnackBar(content: Text(e.message.toString())));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            e.message!.endsWith('by the user.')
+                ? 'Entered OTP is invalid'
+                : 'Something went wrong',
+          ),
+        ),
+      );
     }
   }
 
@@ -112,8 +118,9 @@ class _PhoneAuthWidgetState extends State<PhoneAuthWidget> {
                 vertical: screenUtil.setHeight(16),
               ),
               prefix: Padding(
-                padding:
-                    EdgeInsets.symmetric(horizontal: screenUtil.setWidth(8)),
+                padding: EdgeInsets.symmetric(
+                  horizontal: screenUtil.setWidth(8),
+                ),
                 child: Text(
                   '+91',
                   style: TextStyle(
@@ -156,7 +163,7 @@ class _PhoneAuthWidgetState extends State<PhoneAuthWidget> {
             padding: EdgeInsets.only(bottom: screenUtil.setHeight(141)),
             child: ElevatedButton(
               onPressed: () async {
-                if(phoneController.text.length == 10){
+                if (phoneController.text.length == 10) {
                   setState(() {
                     showLoading = true;
                   });
@@ -164,36 +171,44 @@ class _PhoneAuthWidgetState extends State<PhoneAuthWidget> {
                   print(phoneController.text.toString());
 
                   await _auth.verifyPhoneNumber(
-                    phoneNumber: '+91' + phoneController.text,
-                    verificationCompleted: (phoneAuthCredential) async {
-                      setState(() {
-                        showLoading = false;
-                      });
-                      //signInWithPhoneAuthCredential(phoneAuthCredential);
-                    },
-                    verificationFailed: (verificationFailed) async {
-                      setState(() {
-                        showLoading = false;
-                      });
-                      _scaffoldKey.currentState!.showSnackBar(SnackBar(
-                          content:
-                              Text(verificationFailed.message.toString())));
-                    },
-                    codeSent: (verificationId, resendingToken) async {
-                      setState(() {
-                        showLoading = false;
-                        currentState =
-                            MobileVerificationState.SHOW_OTP_FORM_STATE;
-                        this.verificationId = verificationId;
-                      });
-                    },
-                    codeAutoRetrievalTimeout: (verificationId) async {},
-                    timeout: Duration(seconds: 15)
-                  );
+                      phoneNumber: '+91' + phoneController.text,
+                      verificationCompleted: (phoneAuthCredential) async {
+                        setState(() {
+                          showLoading = false;
+                        });
+                        //signInWithPhoneAuthCredential(phoneAuthCredential);
+                      },
+                      verificationFailed: (verificationFailed) async {
+                        setState(() {
+                          showLoading = false;
+                        });
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              verificationFailed.message.toString(),
+                            ),
+                          ),
+                        );
+                      },
+                      codeSent: (verificationId, resendingToken) async {
+                        setState(() {
+                          showLoading = false;
+                          currentState =
+                              MobileVerificationState.SHOW_OTP_FORM_STATE;
+                          this.verificationId = verificationId;
+                        });
+                      },
+                      codeAutoRetrievalTimeout: (verificationId) async {},
+                      timeout: Duration(seconds: 15));
                 } else {
-                  _scaffoldKey.currentState!.showSnackBar(SnackBar(
-                      content:
-                      Text('Please enter a valid number')));
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      elevation: 8,
+                      content: Text(
+                        'Please enter a valid number',
+                      ),
+                    ),
+                  );
                 }
               },
               child: Container(
@@ -350,154 +365,3 @@ class _PhoneAuthWidgetState extends State<PhoneAuthWidget> {
         ));
   }
 }
-
-/*
-Container(
-      padding: EdgeInsets.symmetric(horizontal: screenUtil.setWidth(24)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: screenUtil.setHeight(15),
-          ),
-          Text(
-            'Enter your mobile number\n to get started',
-            style: TextStyle(
-              fontSize: 24.sp,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          SizedBox(
-            height: screenUtil.setHeight(24),
-          ),
-          TextField(
-            style: TextStyle(
-              fontWeight: FontWeight.w400,
-              fontSize: 22.sp,
-            ),
-            keyboardType: TextInputType.number,
-            inputFormatters: <TextInputFormatter>[
-              FilteringTextInputFormatter.digitsOnly
-            ],
-            maxLength: 10,
-            decoration: InputDecoration(
-              counterText: '',
-              isDense: true,
-              border: OutlineInputBorder(),
-              hintText: '99999 99999',
-              hintStyle: TextStyle(
-                color: ThemeColors.hintTextColor,
-                fontWeight: FontWeight.w400,
-                fontSize: 22.sp,
-              ),
-              labelText: 'Enter your Number',
-              contentPadding: EdgeInsets.symmetric(
-                horizontal: screenUtil.setWidth(24),
-                vertical: screenUtil.setHeight(16),
-              ),
-              prefix: Padding(
-                padding:
-                    EdgeInsets.symmetric(horizontal: screenUtil.setWidth(8)),
-                child: Text(
-                  '+91',
-                  style: TextStyle(
-                    color: ThemeColors.black,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 22.sp,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: screenUtil.setHeight(21),
-          ),
-          Text.rich(
-            TextSpan(
-              text: 'By continuing you are agreeing to our\n',
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: 13.sp,
-              ),
-              children: [
-                TextSpan(
-                  text: 'Terms & Conditions',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 13.sp,
-                    color: ThemeColors.primaryColorLight,
-                  ),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () async => await canLaunch(_url)
-                        ? await launch(_url)
-                        : throw 'Could not launch $_url',
-                ),
-              ],
-            ),
-          ),
-          Spacer(),
-          Padding(
-            padding: EdgeInsets.only(bottom: screenUtil.setHeight(141)),
-            child: ElevatedButton(
-              onPressed: () async {
-                setState(() {
-                  showLoading = true;
-                });
-              },
-              child: Container(
-                width: screenUtil.setWidth(336),
-                height: screenUtil.setHeight(50),
-                alignment: Alignment.center,
-                child: Text(
-                  'GET OTP',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 19.sp,
-                  ),
-                ),
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(
-                  ThemeColors.primaryColorLight,
-                ),
-                elevation: MaterialStateProperty.all(0),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
- */
-
-/*
-() async {
-            setState(() {
-              showLoading = true;
-            });
-
-            await _auth.verifyPhoneNumber(
-              phoneNumber: phoneController.text,
-              verificationCompleted: (phoneAuthCredential) async {
-                setState(() {
-                  showLoading = false;
-                });
-                //signInWithPhoneAuthCredential(phoneAuthCredential);
-              },
-              verificationFailed: (verificationFailed) async {
-                setState(() {
-                  showLoading = false;
-                });
-                _scaffoldKey.currentState!.showSnackBar(
-                    SnackBar(content: Text(verificationFailed.message.toString())));
-              },
-              codeSent: (verificationId, resendingToken) async {
-                setState(() {
-                  showLoading = false;
-                  currentState = MobileVerificationState.SHOW_OTP_FORM_STATE;
-                  this.verificationId = verificationId;
-                });
-              },
-              codeAutoRetrievalTimeout: (verificationId) async {},
-            );
-          },
- */
